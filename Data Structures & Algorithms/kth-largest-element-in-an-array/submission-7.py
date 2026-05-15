113f246524467a395db924afaef9555e86ef7daa
Mai-Linh Vu <160807746+Chezplaty@users.noreply.github.com>
2026-05-15 03:38:42 +0000

    Add: kth-largest-element-in-an-array - submission-7

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-7.py
@@ -0,0 +1,27 @@
+#quickSelect O(n) average, but O(n^2) worse case
+#while loop instead of recursion bc of max recursion limit
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            pivot, p = nums[r], l
+
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            
+            nums[p], nums[r] = pivot, nums[p]
+
+            if k < p:
+                r = p - 1
+            elif k > p:
+                l = p + 1
+            else:
+                return nums[p]
+
+        return nums[l]
+                
