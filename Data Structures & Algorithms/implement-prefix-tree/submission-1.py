f593e1c4af4725e2c98fd1839007b7a0aaf1334f
Mai-Linh Vu <160807746+Chezplaty@users.noreply.github.com>
2026-07-27 22:11:17 -0700

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,23 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.words = set()
+        
+
+    def insert(self, word: str) -> None:
+        self.words.add(word)
+        self.prev = word
+
+
+    def search(self, word: str) -> bool:
+        return word in self.words
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        for word in self.words:
+            if word[:len(prefix)] == prefix:
+                return True
+
+        return False
+        
+        
